0e9664191d63b8ff8557a05436ceeb0d01f971dd
Popa Laurentiu <popa.laur.01@gmail.com>
2024-10-24 11:33:39 +0300

    Projects card created

--- lp-portfolio/src/components/Right/Projects.tsx
+++ lp-portfolio/src/components/Right/Projects.tsx
@@ -1,6 +1,44 @@
+import WorkCard from "./WorkCard"
+
+const work:{
+  image:string;
+  subject:string;
+  link:string;
+  description:string;
+  skills:string[]}[] = [
+  {
+    "image" :"2023 - PRESENT",
+    "subject" : "Master in Software Development and Business Information Systems",
+    "link":"",
+    "description" : "Learned a lot of things iaibal",
+    "skills" : ["Team work", "BI"]
+  },
+  {
+    "image" :"2023 - PRESENT",
+    "subject" : "Master in Software Development and Business Information Systems",
+    "link":"",
+    "description" : "Learned a lot of things iaibal",
+    "skills" : ["Team work", "BI"]
+  },
+  {
+    "image" :"2023 - PRESENT",
+    "subject" : "Master in Software Development and Business Information Systems",
+    "link":"",
+    "description" : "Learned a lot of things iaibal",
+    "skills" : ["Team work", "BI"]
+  },
+]
+
 
 export default function Projects() {
+
+  const Work = work.map(i => (
+    <WorkCard image = {i.image} subject = {i.subject} link = {i.subject} description={i.description} skills={i.skills}/>
+  ))
+
   return (
-    <div>Projects</div>
+    <div>
+      {Work}
+    </div>
   )
 }
---
+++ lp-portfolio/src/components/Right/WorkCard.tsx
@@ -0,0 +1,22 @@
+interface Work {
+    image:string;
+    subject:string;
+    link:string;
+    description:string;
+    skills:string[]
+}
+
+export default function WorkCard({image, subject, link, description, skills}:Work) {
+    return (
+        <div className="flex items-start ">
+            <div className="w-[30%] txt">
+                <img src={image} />
+            </div>
+            <div className="w-[70%]">
+                <p className="text-xl">{subject}</p>
+                <p className="txt">{description}</p>
+                <p className="text-[#fcd34d]">{skills}</p>
+            </div>
+        </div>
+    )
+}
